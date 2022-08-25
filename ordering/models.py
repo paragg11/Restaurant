21ee00860fc712e6d9a760fed4697b7827a8c9e1
paragg19 <parag.gunjal@sparkeighteen.com>
2022-08-25 14:35:34 +0530

    latest work

--- ordering/models.py
+++ ordering/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from menu.models import RestaurantMenu
 
 # Create your models here.
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        RECIEVED = "RECIEVED"
+        ACCEPTED = "ACCEPTED"
+        PREPARING = "PREPARING"
+        PREPARPED = "PREPARED"
+        IN_TRANSIT = "IN_TRANSIT"
+        DELIVERED = "DELIVERED"
+        CANCELED = "CANCELED"
+
+orderstatus = models.CharField(max_length=15, choices=orderstatus.choices)
+timestamp = models.DateTimeField(auto_now_add=True)
